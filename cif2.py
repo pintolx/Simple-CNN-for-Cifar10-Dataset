9a832a378be4080ac19c4efcdee5c29b0b60fad8
Byronlx <byronkats96@gmail.com>
2017-06-10 13:00:05 -0400

    Add files via upload

---
+++ cif2.py
@@ -0,0 +1,53 @@
+#Simple CNN model for CIFAR-10
+import numpy
+from keras.datasets import cifar10
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.layers import Flatten
+from keras.layers.convolutional import MaxPooling2D
+from keras.layers.convolutional import Conv2D
+from keras.constraints import maxnorm
+from keras.optimizers import SGD
+from keras import backend as K 
+from keras.utils import np_utils
+
+#Setting image Dimensions
+K.set_image_dim_ordering('th')
+
+#Fix random seed for reproducibility
+seed = 7
+numpy.random.seed(seed)
+
+#Loading the datasets
+(X_train, y_train), (X_test, y_test) = cifar10.load_data()
+
+#Converting the data to floats and normalizing the dataset 
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
+X_train = X_train/255.0
+X_test = X_test/255.0
+
+#One hot encode outputs
+y_train = np_utils.to_categorical(y_train)
+y_test = np_utils.to_categorical(y_test)
+num_classes = y_test.shape[1]
+
+#Creating the model
+model = Sequential()
+model.add(Conv2D(32, (3, 3), input_shape=(3, 32, 32), padding='same', activation='relu', kernel_constraint=maxnorm(3)))
+model.add(Dropout(0.2))
+model.add(Conv2D(32, (3, 3), padding='same', activation='relu', kernel_constraint=maxnorm(3)))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Flatten())
+model.add(Dense(512, activation='relu', kernel_constraint=maxnorm(3)))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes, activation='softmax'))
+
+#Compile the model
+epochs = 25
+lrate = 0.01
+decay = lrate/epochs
+sgd = SGD(lr=lrate, momentum=0.9, decay=decay,nesterov=False)
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+print(model.summary())
